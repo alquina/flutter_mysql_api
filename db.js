5e1d0eda67c7a7ae39e691bd93400fdf1eabd670
Demitri <demitri@example.com>
2024-11-24 01:49:28 +0800

    Initial commit

---
+++ db.js
@@ -0,0 +1,18 @@
+const mysql = require('mysql2');
+
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+});
+
+connection.connect((err) => {
+  if (err) {
+    console.error('Error connecting to MySQL:', err.message);
+    process.exit(1);
+  }
+  console.log('Connected to MySQL database!');
+});
+
+module.exports = connection;
